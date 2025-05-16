f815238d2bd8aee26188f500975b71b95e352192
Saša Barišić <sasa.barisic@serengetitech.com>
2025-05-16 14:45:43 +0200

    Parsiranje Lidl podataka

--- Importer/Importer_Lidl.cs
+++ Importer/Importer_Lidl.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using CijeneAPI.Model;
+using System;
 using System.Collections.Generic;
 using System.IO.Compression;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+using TinyCsvParser;
+using TinyCsvParser.Mapping;
 
 namespace CijeneAPI.Importer {
 	class Importer_Lidl : CijeneImporter {
@@ -41,19 +45,57 @@ namespace CijeneAPI.Importer {
 			using (ZipArchive Zip = new ZipArchive(ZipStream, ZipArchiveMode.Read)) {
 				foreach (var E in Zip.Entries) {
 					Console.WriteLine(">>>> {0}", E.FullName);
-					string CSV_Contents = "";
 
-					using (Stream S = E.Open()) {
-						CSV_Contents = Encoding.Unicode.GetString(S.ToByteArray());
+					//string CSV_Contents = "";
+					//byte[] CSV_Bytes = E.Open().ToByteArray();
+					//CSV_Contents = Encoding.ASCII.GetString(CSV_Bytes);
 
-						ParseCSV(E.FullName, CSV_Contents, DT);
-					}
+					//CSV_Contents = Encoding.Default.GetString(CSV_Bytes);
+
+					ParseCSV(E.FullName, E.Open(), DT);
+
 				}
 			}
 		}
 
-		void ParseCSV(string CSVName, string CSVSrc, DateTime DT) {
+		void ParseCSV(string CSVName, Stream CSVSrc, DateTime DT) {
+			CsvParserOptions CsvParserOpt = new CsvParserOptions(true, ',');
+			CsvLidlMapping LidlMapping = new CsvLidlMapping();
+			CsvParser<CijeneAPIEntry> CsvParser = new CsvParser<CijeneAPIEntry>(CsvParserOpt, LidlMapping);
 
+			string[] NameTokens = CSVName.Split("_").ToArray();
+			CsvMappingResult<CijeneAPIEntry>[] MappingResult = CsvParser.ReadFromStream(CSVSrc, Encoding.ASCII, true).ToArray();
+
+			foreach (CsvMappingResult<CijeneAPIEntry> MR in MappingResult) {
+				CijeneAPIEntry Entry = MR.Result;
+				Entry.Store = "Lidl";
+				Entry.Location = NameTokens[1];
+
+				string[] AddrTokens = new string[NameTokens.Length - 4];
+				Array.Copy(NameTokens, 2, AddrTokens, 0, AddrTokens.Length);
+
+				Entry.Address = string.Join(' ', AddrTokens);
+				Entries.Add(Entry);
+			}
+		}
+	}
+
+	class CsvLidlMapping : CsvMapping<CijeneAPIEntry> {
+		public CsvLidlMapping()
+			: base() {
+
+			int Cnt = 0;
+			MapProperty(Cnt++, x => x.Naziv);
+			MapProperty(Cnt++, x => x.Sifra);
+			MapProperty(Cnt++, x => x.NetoKolicina);
+			MapProperty(Cnt++, x => x.JedinicaMjere);
+			MapProperty(Cnt++, x => x.Pakiranje);
+			MapProperty(Cnt++, x => x.Marka);
+			MapProperty(Cnt++, x => x.MaloprodajnaCijena);
+			MapProperty(Cnt++, x => x.CijenaZaJedinicuMjere);
+			MapProperty(Cnt++, x => x.Barkod);
+			MapProperty(Cnt++, x => x.KategorijaProizvoda);
+			MapProperty(Cnt++, x => x.SidrenaCijena);
 		}
 	}
 }
--- Model/CijeneAPIEntry.cs
+++ Model/CijeneAPIEntry.cs
@@ -8,11 +8,54 @@ namespace CijeneAPI.Model {
 	public class CijeneAPIEntry {
 		public string Store;
 		public string Location;
+		public string Address;
 
-		public string ProductName;
+		public string Naziv {
+			get; set;
+		}
 
-		public CijeneAPIEntry() {
+		public string Sifra {
+			get; set;
+		}
 
+		public string NetoKolicina {
+			get; set;
+		}
+
+		public string JedinicaMjere {
+			get; set;
+		}
+
+		public string Pakiranje {
+			get; set;
+		}
+
+		public string Marka {
+			get; set;
+		}
+
+		public string MaloprodajnaCijena {
+			get; set;
+		}
+
+		public string CijenaZaJedinicuMjere {
+			get; set;
+		}
+
+		public string Barkod {
+			get; set;
+		}
+
+		public string KategorijaProizvoda {
+			get; set;
+		}
+
+		public string SidrenaCijena {
+			get; set;
+		}
+
+		public override string ToString() {
+			return string.Format("[{0} {1}] - {2}, {3} EUR", Store, Location, Naziv, MaloprodajnaCijena);
 		}
 	}
 }
